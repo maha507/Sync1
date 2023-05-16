e5d346090305a7c0d028c05cd8f06f0907ae0793
maha507 <132257569+maha507@users.noreply.github.com>
2023-05-16 15:48:55 +0530

    commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,25 @@
-<h1>hello</h1>
+<script>
+  import { onMount } from "svelte";
+  import Grid from "gridjs-svelte"
+  const endpoint = "https://jsonplaceholder.typicode.com/posts";
+  let posts = [];
+
+  onMount(async function () {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log(data);
+  });
+  
+  export let name;
+</script>
+
+<main>
+  <h1>click the data</h1>
+  <p>Visit the <a href="https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3">data</a> for more details.</p>
+
+  {#each posts as article}
+    <div>
+      <p>{article.title}</p>
+    </div>
+  {/each}
+</main>
